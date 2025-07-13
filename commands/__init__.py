a53ada7bb66315bcbba843fc026c3cb89c77756b
Kevin Schneider <schneik80@gmail.com>
2025-07-13 13:44:31 -0700

    remove references command

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,14 +1,16 @@
-from .refrences import entry as refrences
+# from .refrences import entry as refrences
 from .relateddata import entry as relateddata
 
 commands = [
-    refrences,
+    # refrences,
     relateddata,
 ]
+
 
 def start():
     for command in commands:
         command.start()
+
 
 def stop():
     for command in commands:
